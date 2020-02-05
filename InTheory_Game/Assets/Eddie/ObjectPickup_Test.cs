e8150f0d82aa5af1db53fcc18a763b9db0af29e6
EddieTHilton <58077810+EddieTHilton@users.noreply.github.com>
2020-02-05 19:18:02 +0000

    Update ObjectPickup_Test.cs

--- InTheory_Game/Assets/Eddie/ObjectPickup_Test.cs
+++ InTheory_Game/Assets/Eddie/ObjectPickup_Test.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ObjectPickup_Test : MonoBehaviour
 {
+
+    private GameObject DruidCarryContainer; // The Cary Container empty object attached to the player controller in the scene.
+    private Transform CarryPos; // Position of the carry container.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DruidCarryContainer = GameObject.Find("Carried_Object_Container");
+        CarryPos = DruidCarryContainer.transform;  // Sets up the appropriate objects and transforms for later in the script.
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float dist = Vector3.Distance(CarryPos.position, transform.position);
+        if (dist <= 2)  // tests distance between this object and the cary container
+        {
+
+        }
     }
 }
